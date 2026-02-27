dd07af330818d3ed72eae7d36bd67e177cf251a9
rushda628 <saiyedrushdaz@gmail.com>
2026-02-27 11:31:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    let reveals = document.querySelectorAll('.reveal');
+
+    for(let i=0; i<reveals.length; i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 100;
+
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active');
+        }
+    }
+}
